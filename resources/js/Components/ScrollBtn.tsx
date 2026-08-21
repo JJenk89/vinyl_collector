064f08349ff86713be291751e0f5858d845b457f
jjenk <j.jenkinson89@hotmail.com>
2026-08-21 14:50:45 +0200

    feat (ScrollBtn) - add: useEffect and state to component.
    This makes the scroll to top button visible only after a page Y offset improving UX

--- resources/js/Components/ScrollBtn.tsx
+++ resources/js/Components/ScrollBtn.tsx
@@ -1,4 +1,27 @@
+import { useState, useEffect } from 'react';
+
 const ScrollToTop = () => {
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const toggleVisibility = () => {
+            if (window.pageYOffset > 400) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        };
+
+        window.addEventListener('scroll', toggleVisibility);
+
+        return () => window.removeEventListener('scroll', toggleVisibility);
+    }, []);
+
+    if (!isVisible) {
+        return null;
+    }
+
     return ( 
         <button
             onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
